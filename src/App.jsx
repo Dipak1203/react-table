28885e6cc82b0202cc11fef1ecc9d32095893e45
DipakDai <dipakkumal673@gmail.com>
2023-05-04 13:22:53 +0545

    react-table

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css';
+import Table from './components/Table.jsx';
+const App = () => {
+  return (
+    <div>
+      <Table />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Table.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import fakeData from './Data.json';
+import {useTable} from 'react-table'
+const Table = () => {
+    const data = React.useMemo(() => fakeData,[]);  
+    const columns = React.useMemo(() => [
+        {
+            Header:"ID",
+            accessor:"id"
+        },
+        {
+            Header:"First Name",
+            accessor:"first_name"
+        },
+        {
+            Header:"Last Name",
+            accessor:"last_name"
+        },
+        {
+            Header:"Email",
+            accessor:"email"
+        },
+        {
+            Header:"Gender",
+            accessor:"gender"
+        },
+        {
+            Header:"University",
+            accessor:"university"
+        },
+    ],[]);
+
+    const {getTableProps,getTableBodyProps,headerGroups,rows,prepareRow} = useTable({columns,data})
+  return (    
+    <div className="App">
+      <div className="container">
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()}>
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => (
+                    <td {...cell.getCellProps()}> {cell.render("Cell")} </td>
+                  ))}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
+
+export default Table
